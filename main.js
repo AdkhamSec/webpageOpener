8e72a52b4aa5e4fbb96b56ef112d14d281831185
AdkhamSec <adkhamsec@gmail.com>
2024-01-18 19:36:25 +0500

    fullscreen

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    fullscreen: true,
+    autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: true,
     },
